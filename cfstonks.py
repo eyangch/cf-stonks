45d0874c1fba3fe22e508dd1a2cee8223fbd5867
eyangch <eyangch@gmail.com>
2021-01-08 20:30:18 -0600

    Add backend and webpages

---
+++ cfstonks.py
@@ -0,0 +1,212 @@
+from flask import Flask, request, jsonify
+import json
+import hashlib
+from urllib.request import urlopen
+import os
+
+app = Flask(__name__)
+
+def c_fsize():
+    fsize = os.path.getsize("login.json") + os.path.getsize("data.json")
+    if fsize > 1000000:
+        return True
+    return False
+
+@app.after_request
+def after_request(response):
+    response.headers["Access-Control-Allow-Origin"] = '*'
+    return response
+
+def getRating(handle):
+    try:
+        with urlopen("https://codeforces.com/api/user.info?handles={}".format(handle)) as r:
+            h_info = r.read()
+    except:
+        return -1
+    h_info = json.loads(h_info)
+    if h_info["status"] != "OK":
+        return -1
+    if "rating" not in h_info["result"][0]:
+        return -1
+    return int(h_info["result"][0]["rating"])
+
+def getPrice(rating):
+    return 1.002 ** rating
+
+@app.route("/reg", methods = ["GET"])
+def reg():
+    if c_fsize():
+        return "0"
+    user = request.args.get("u")
+    pwd = request.args.get("p")
+    print("{} has registered".format(user))
+    if user == "" or pwd == "" or user == None or pwd == None:
+        return "0";
+
+    logdat = {}
+    with open("login.json") as f:
+        logdat = json.load(f)
+    if user in logdat:
+        return "0"; 
+    logdat[user] = hashlib.sha256(pwd.encode()).hexdigest();
+    with open("login.json", "w") as f:
+        json.dump(logdat, f);
+
+    udat = {}
+    with open("data.json") as f:
+        udat = json.load(f)
+    with open("data.json", "w") as f:
+        udat[user] = {"$": 4000.0, "stonk": {}}
+        json.dump(udat, f);
+
+    return "1"
+
+@app.route("/login", methods = ["GET"])
+def login():
+    if c_fsize():
+        return "0"
+    user = request.args.get("u")
+    pwd = request.args.get("p")
+    print("{} is trying to log in".format(user))
+
+    if user == "" or pwd == "" or user == None or pwd == None:
+        return "0";
+
+    logdat = {}
+    with open("login.json") as f:
+        logdat = json.load(f)
+    if user not in logdat:
+        return "0"; 
+    
+    if logdat[user] != hashlib.sha256(pwd.encode()).hexdigest():
+        return "0"
+
+    return "1"
+
+@app.route("/get", methods = ["GET"])
+def get():
+    if c_fsize():
+        return "0"
+    user = request.args.get("u")
+    print("Retrieve {}".format(user))
+    if user == "" or user == None:
+        return "0"
+    
+    udat = {}
+    with open("data.json") as f:
+        udat = json.load(f)
+
+    if user not in udat:
+        return "0"
+
+    return jsonify(udat[user])
+
+@app.route("/invest", methods = ["GET"])
+def invest():
+    if c_fsize():
+        return "0"
+    handle = request.args.get("handle").lower()
+    user = request.args.get("u")
+    pwd = request.args.get("p")
+    amt = request.args.get("amt")
+    
+    try:
+        amt = int(amt)
+    except:
+        return "0"
+
+    print("{} investing in {}".format(user, handle))
+    if user == "" or pwd == "" or user == None or pwd == None:
+        return "0"
+
+    logdat = {}
+    with open("login.json") as f:
+        logdat = json.load(f)
+    if user not in logdat:
+        return "0"
+    
+    if logdat[user] != hashlib.sha256(pwd.encode()).hexdigest():
+        return "0"
+    
+    rating = getRating(handle)
+    if rating == -1:
+        return "0"
+
+    price = getPrice(rating)
+
+    print("{} has a rating of {}, costing {}".format(handle, rating, price))
+    
+    udat = {}
+    with open("data.json") as f:
+        udat = json.load(f)
+    
+    if amt * price > udat[user]["$"]:
+        return "0"
+    udat[user]["$"] -= amt * price
+    
+    if handle not in udat[user]["stonk"]:
+        udat[user]["stonk"][handle] = 0
+    udat[user]["stonk"][handle] += amt
+
+    with open("data.json", "w") as f:
+        json.dump(udat, f)
+
+    return "1"
+
+@app.route("/sell", methods = ["GET"])
+def sell():
+    if c_fsize():
+        return "0"
+    handle = request.args.get("handle").lower()
+    user = request.args.get("u")
+    pwd = request.args.get("p")
+    amt = request.args.get("amt")
+    
+    try:
+        amt = int(amt)
+    except:
+        return "0"
+
+    print("{} selling {}".format(user, handle))
+    if user == "" or pwd == "" or user == None or pwd == None:
+        return "0"
+
+    logdat = {}
+    with open("login.json") as f:
+        logdat = json.load(f)
+    if user not in logdat:
+        return "0"
+    
+    if logdat[user] != hashlib.sha256(pwd.encode()).hexdigest():
+        return "0"
+    
+    rating = getRating(handle)
+    if rating == -1:
+        return "0"
+
+    price = getPrice(rating)
+
+    print("{} has a rating of {}, costing {}".format(handle, rating, price))
+    
+    udat = {}
+    with open("data.json") as f:
+        udat = json.load(f)
+    
+    if handle not in udat[user]["stonk"]:
+        return "0"
+
+    if amt > udat[user]["stonk"][handle]:
+        return "0"
+    udat[user]["$"] += amt * price
+    
+    udat[user]["stonk"][handle] -= amt
+    if udat[user]["stonk"][handle] == 0:
+        del udat[user]["stonk"][handle]
+
+    with open("data.json", "w") as f:
+        json.dump(udat, f)
+
+    return "1"
+
+if __name__ == "__main__":
+    app.run()
---
+++ docs/index.js
@@ -0,0 +1,151 @@
+function h_sha256(message){
+    return sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(message));
+}
+
+var user = localStorage.getItem("user");
+var pass = localStorage.getItem("pass");
+
+var logged_in = true;
+
+var networth = 0
+
+if(user === null || pass === null){
+    logged_in = false;
+}
+
+if(logged_in){
+    function init_response(){
+        //console.log(this.responseText);
+        if(this.responseText == "0"){
+            logged_in = false;
+            localStorage.removeItem("user");
+            localStorage.removeItem("pass");
+            location.reload();
+        }
+    }
+    var xmlreq1 = new XMLHttpRequest();
+    xmlreq1.addEventListener("load", init_response);
+    xmlreq1.open("GET", "https://cfstonks.loca.lt/login?u=" + user + "&p=" + pass);
+    xmlreq1.send();
+}
+
+function getMoney(val){
+    return (Math.round(val * 100) / 100).toString();
+}
+
+function incNet(val){
+    networth += val
+    document.getElementById("networth").innerHTML = getMoney(networth);
+}
+
+if(logged_in){
+    document.getElementById("signed-out").innerHTML = "";
+    document.getElementById("username").innerHTML = user;
+    function get_response(){
+        var user_data = JSON.parse(this.responseText);
+        //console.log(user_data);
+        document.getElementById("money").innerHTML = getMoney(user_data["$"]);
+        incNet(user_data["$"]);
+        for(let handle in user_data["stonk"]){
+            function cf_response(){
+                cf_data = JSON.parse(this.responseText);
+                rating = cf_data["result"][0]["rating"];
+                price = Math.pow(1.002, rating);
+                chandle = cf_data["result"][0]["handle"].toLowerCase();
+                //console.log(user_data["stonk"][chandle].toString() + " stocks of " + chandle + " at " + price.toString() + " each (rated " + rating.toString() + ")");
+                incNet(price * user_data["stonk"][chandle]);
+                document.getElementById("stocks").innerHTML += user_data["stonk"][chandle].toString() + " stocks of " + chandle + " at " + getMoney(price) + " each (rated " + rating.toString() + ")</br>"
+            }
+            var cfreq = new XMLHttpRequest();
+            cfreq.addEventListener("load", cf_response);
+            cfreq.open("GET", "https://codeforces.com/api/user.info?handles=" + handle);
+            cfreq.send();
+        }
+    }
+    var xmlreq1 = new XMLHttpRequest();
+    xmlreq1.addEventListener("load", get_response);
+    xmlreq1.open("GET", "https://cfstonks.loca.lt/get?u=" + user);
+    xmlreq1.send(); 
+}else{
+    document.getElementById("signed-in").innerHTML = "";
+}
+
+function register(){
+    function reg_response(){
+        //console.log(this.responseText);
+        if(this.responseText == "0"){
+            alert("Error in registering.");
+            return;
+        }else{
+            login();
+        }
+    }
+    reg_user = document.getElementById("sign-username").value;
+    reg_pass = document.getElementById("sign-password").value;
+    var regreq = new XMLHttpRequest();
+    regreq.addEventListener("load", reg_response);
+    //console.log(h_sha256(reg_pass));
+    regreq.open("GET", "https://cfstonks.loca.lt/reg?u=" + reg_user + "&p=" + h_sha256(reg_pass));
+    regreq.send();
+}
+
+function login(){
+    login_user = document.getElementById("sign-username").value;
+    login_pass = document.getElementById("sign-password").value;
+    function login_response(){
+        //console.log(this.responseText);
+        if(this.responseText == "0"){
+            alert("Error logging in. (Maybe incorrect credentials)");
+            return;
+        }else{
+            localStorage.setItem("user", login_user);
+            localStorage.setItem("pass", h_sha256(login_pass));
+            location.reload();
+        }
+    }
+    var regreq = new XMLHttpRequest();
+    regreq.addEventListener("load", login_response);
+    //console.log(h_sha256(login_pass));
+    regreq.open("GET", "https://cfstonks.loca.lt/login?u=" + login_user + "&p=" + h_sha256(login_pass));
+    regreq.send();
+}
+
+function logout(){
+    localStorage.removeItem("user");
+    localStorage.removeItem("pass");
+    location.reload();
+}
+
+function invest(){
+    handle = document.getElementById("handle").value;
+    amt = document.getElementById("amt").value;
+    function invest_response(){
+        //console.log(this.responseText);
+        if(this.responseText == "0"){
+            alert("There was a problem investing");
+        }else{
+            location.reload();
+        }
+    }
+    var xmlreq1 = new XMLHttpRequest();
+    xmlreq1.addEventListener("load", invest_response);
+    xmlreq1.open("GET", "https://cfstonks.loca.lt/invest?u=" + user + "&p=" + pass + "&handle=" + handle + "&amt=" + amt);
+    xmlreq1.send();
+}
+
+function sell(){
+    handle = document.getElementById("sellhandle").value;
+    amt = document.getElementById("sellamt").value;
+    function sell_response(){
+        //console.log(this.responseText);
+        if(this.responseText == "0"){
+            alert("There was a problem selling");
+        }else{
+            location.reload();
+        }
+    }
+    var xmlreq1 = new XMLHttpRequest();
+    xmlreq1.addEventListener("load", sell_response);
+    xmlreq1.open("GET", "https://cfstonks.loca.lt/sell?u=" + user + "&p=" + pass + "&handle=" + handle + "&amt=" + amt);
+    xmlreq1.send();
+}
